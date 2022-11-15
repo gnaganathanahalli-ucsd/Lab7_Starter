920c46e9bd096c8fcb71580158687061b99c8406
gnamithan <gnaganathanahalli@ucsd.edu>
2022-11-14 23:52:54 -0800

    part 2 and readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab 7 - Starter
+Gnamitha Naganathanahalli
+https://gnaganathanahalli-ucsd.github.io/Lab7_Starter/
--- sw.js
+++ sw.js
@@ -9,6 +9,14 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
+      const RECIPE_URLS = [
+        'https://introweb.tech/assets/json/1_50-thanksgiving-side-dishes.json',
+        'https://introweb.tech/assets/json/2_roasting-turkey-breast-with-stuffing.json',
+        'https://introweb.tech/assets/json/3_moms-cornbread-stuffing.json',
+        'https://introweb.tech/assets/json/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+        'https://introweb.tech/assets/json/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+        'https://introweb.tech/assets/json/6_one-pot-thanksgiving-dinner.json',
+      ];
       return cache.addAll([]);
     })
   );
@@ -34,7 +42,24 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
+  // Open the cache
+  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+    // Respond with the image from the cache or from the network
+    return cache.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request.url).then((fetchedResponse) => {
+        cache.put(event.request, fetchedResponse.clone());
+
+        // Return the network response
+        return fetchedResponse;
+      });
+    });
+  }));
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
+  //// if request is in the cache, return with the cached version
+  //// else, 
+  //// fetch the resource
+  //// add it to the cache
+  //// return the network response
 });
